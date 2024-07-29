245c92a6d94aa43ab5deb85843cf0db6a29ab934
tweichuan <wctsai20002@gmail.com>
2024-07-29 17:36:23 +0800

    Fix package

--- repository2prompt/__init__.py
+++ repository2prompt/__init__.py
@@ -2,4 +2,4 @@ from .repository2prompt import Repository2Prompt
 from .config import CONFIG
 from .utils import github_api, input_handler, file_processor, output_formatter
 
-__all__ = ['Repository2Prompt', 'CONFIG', 'github_api', 'input_handler', 'file_processor', 'output_formatter']
+__all__ = ['Repository2Prompt', 'CONFIG']
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wctsai20002/repository2prompt",
-    packages=find_packages(),
+    packages=find_packages(include=['repository2prompt', 'repository2prompt.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
